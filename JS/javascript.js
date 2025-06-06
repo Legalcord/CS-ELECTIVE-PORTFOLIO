f323c3f02b0750f2f07ee96b172b34394b077509
Michael Quinones <ellevincent304@gmail.com>
2025-06-06 14:27:32 +0800

    Fifth commit

--- JS/javascript.js
+++
@@ -1,8 +0,0 @@
-
-const navLinks = document.querySelectorAll('.navbar').
-navLinks.forEach(navLink =>{
-    navLink.addEventListener('click', () => {
-        document.querySelector('.active').classList.remove('active');
-        navLink.classList.add('active');
-    })
-})
